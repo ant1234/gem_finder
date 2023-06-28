d4169589765195428c73f248d285178971211985
unknown <antonyvidetta1986@gmail.com>
2023-06-28 13:20:26 +1200

    working calendar

---
+++ src/components/Expenses/NewExpenseComponent.js
@@ -0,0 +1,30 @@
+import './Expenses/NewExpenseComponent.css';
+import ExpenseItem from './Expenses/ExpenseItem';
+import Card from '../UI/Card';
+
+const NewExpenseComponent = (props) => {
+
+    return (
+        <Card className='expenses'>
+            <ExpenseItem 
+                title={props.expenses[0].title} 
+                amount={props.expenses[0].amount} 
+                date={props.expenses[0].date}>
+            </ExpenseItem>
+
+            <ExpenseItem 
+                title={props.expenses[1].title} 
+                amount={props.expenses[1].amount} 
+                date={props.expenses[1].date}>
+            </ExpenseItem>
+
+            <ExpenseItem 
+                title={props.expenses[2].title} 
+                amount={props.expenses[2].amount} 
+                date={props.expenses[2].date}>
+            </ExpenseItem>
+        </Card>
+    );
+}
+
+export default NewExpenseComponent;
